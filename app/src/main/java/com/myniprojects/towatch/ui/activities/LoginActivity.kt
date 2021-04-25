73c68809f43b8c95828d4bfdf9bc02708fe80bab
CzarqR <czarekr98@gmail.com>
2021-04-25 23:08:09 +0200

    Splash screen, readme files

--- app/src/main/java/com/myniprojects/towatch/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/myniprojects/towatch/ui/activities/LoginActivity.kt
@@ -28,6 +28,7 @@ class LoginActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_ToWatch)
         setContentView(binding.root)
 
         binding.viewModel = viewModel
--- app/src/main/java/com/myniprojects/towatch/ui/activities/MainActivity.kt
+++ app/src/main/java/com/myniprojects/towatch/ui/activities/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_ToWatch)
         setContentView(binding.root)
 
         setupCollecting()
